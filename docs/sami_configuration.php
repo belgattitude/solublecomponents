8d226f3e78a05782a698862e1bb5424ae17b0158
sebastien <belgattitude@gmail.com>
2014-08-06 18:42:16 +0200

    Updated for PHP 5.6 compat

--- docs/sami_configuration.php
+++ docs/sami_configuration.php
@@ -14,7 +14,9 @@ $iterator = Finder::create()
 return new Sami($iterator, array(
     'title' => 'Soluble API',
     'theme' => 'enhanced',
+    //'theme' => 'symfony',
     'build_dir' => __DIR__.'/sphinx/source/_static/API/SAMI',
     'cache_dir' => __DIR__.'/_build/cache',
     'default_opened_level' => 2,
+    //'template_dirs'        => array(__DIR__.'/themes/symfony'),
 ));
