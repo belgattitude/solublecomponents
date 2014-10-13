75441aa7eb43cbebf0d83ef87cfd50b596875d26
sebastien <belgattitude@gmail.com>
2014-10-13 20:54:58 +0200

    Updated tests

---
+++ tests/phpunit_with_coverage.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+#php -c ./config/php-xdebug.ini /usr/local/bin/phpunit
+php -c ./config/php-xdebug.ini ../vendor/bin/phpunit
--- tests/phpunit_with_xdebug.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-php -c ./config/php-xdebug.ini /usr/local/bin/phpunit
