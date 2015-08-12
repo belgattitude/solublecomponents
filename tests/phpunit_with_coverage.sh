d16b6e87ab6a82ef73216e892f6c23e4b2a2cdf0
sebastien <belgattitude@gmail.com>
2015-08-12 15:06:27 +0200

    Preparation before splitting into separate components

--- tests/phpunit_with_coverage.sh
+++ tests/phpunit_with_coverage.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 #php -c ./config/php-xdebug.ini /usr/local/bin/phpunit
-php -c ./config/php-xdebug.ini ../vendor/bin/phpunit
+#php -c ./config/php-xdebug.ini ../vendor/bin/phpunit
+php -d zend_extension=xdebug.so ../vendor/bin/phpunit
