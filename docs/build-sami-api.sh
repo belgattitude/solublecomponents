1a24d221a03366612fa6eb3186cf312d97ce9461
sebastien <belgattitude@gmail.com>
2015-10-14 12:32:24 +0200

    removed some deps

--- docs/build-sami-api.sh
+++ docs/build-sami-api.sh
@@ -1,1 +1,2 @@
-./../bin/sami.php update ./sami_configuration.php 
+# sudo wget http://get.sensiolabs.org/sami.phar -O /usr/local/bin/sami; sudo chmod +x /usr/local/bin/sami
+/usr/local/bin/sami update ./sami_configuration.php 
